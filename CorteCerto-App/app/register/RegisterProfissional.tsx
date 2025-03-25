46e537cf2d8347b7473e03cc5e80bab54b82c639
Kauan Santana <kauan.silvabr@yahoo.com>
2025-03-25 15:55:18 -0300

    Update RegisterProfissional.tsx

--- CorteCerto-App/app/register/RegisterProfissional.tsx
+++ CorteCerto-App/app/register/RegisterProfissional.tsx
@@ -114,7 +114,7 @@ const Register: React.FC = () => {
       {/* Link para acessar a tela de login */}
       <Text style={styles.loginText}>
         Já tenho uma conta.{" "}
-        <Text style={styles.link} onPress={() => router.push("/SignInProfissional/SingInProfissional")}>
+        <Text style={styles.link} onPress={() => router.push("/register/RegisterProfissional")}>
           Acessar agora!
         </Text>
       </Text>
