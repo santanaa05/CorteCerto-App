aa2274f584fdaa7a521b255a52fff1ff9163c4eb
Kauan Santana <kauan.silvabr@yahoo.com>
2025-03-26 14:50:54 -0300

    Update RegisterEndereço.tsx

--- CorteCerto-App/app/register/RegisterEndereço.tsx
+++ CorteCerto-App/app/register/RegisterEndereço.tsx
@@ -4,7 +4,6 @@ import { useForm, Controller } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Stack } from "expo-router"; // Importa o Stack do expo-router para navegação entre telas
-import { Ionicons } from "@expo/vector-icons"; // Importa os ícones da biblioteca Ionicons
 import { useRouter } from "expo-router"; // Importação do expo-router
 
 const schema = yup.object().shape({
